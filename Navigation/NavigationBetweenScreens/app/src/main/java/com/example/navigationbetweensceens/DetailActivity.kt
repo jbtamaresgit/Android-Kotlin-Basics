48d298c77f463e09048f5b11e4a6f78053446b10
jbtamaresgit <jbtamaresgit@outlook.com>
2021-10-06 16:24:20 +0800

    Implement recyclerview and its adapter for the letters and words

--- Navigation/NavigationBetweenScreens/app/src/main/java/com/example/navigationbetweensceens/DetailActivity.kt
+++ Navigation/NavigationBetweenScreens/app/src/main/java/com/example/navigationbetweensceens/DetailActivity.kt
@@ -1,4 +1,29 @@
 package com.example.navigationbetweensceens
 
-class DetailActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.navigationbetweensceens.adapter.WordAdapter
+import com.example.navigationbetweensceens.databinding.ActivityDetailBinding
+
+class DetailActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityDetailBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val letterId = "A"
+        val recyclerView = binding.recyclerView
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = WordAdapter(letterId,this)
+
+        recyclerView.addItemDecoration(
+            DividerItemDecoration(this, DividerItemDecoration.VERTICAL)
+        )
+
+        title = getString(R.string.detail_prefix) + " " + letterId
+    }
 }
--- Navigation/NavigationBetweenScreens/app/src/main/java/com/example/navigationbetweensceens/MainActivity.kt
+++ Navigation/NavigationBetweenScreens/app/src/main/java/com/example/navigationbetweensceens/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.navigationbetweensceens
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.navigationbetweensceens.adapter.LetterAdapter
+import com.example.navigationbetweensceens.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var recyclerView: RecyclerView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        recyclerView = binding.recyclerView
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = LetterAdapter()
     }
 }
