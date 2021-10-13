9f83ccdc2cb06bf18079fa11dda022e8bba82472
jbtamaresgit <jbtamaresgit@outlook.com>
2021-10-13 17:32:32 +0800

    implement view creation of fragment letter list

--- Navigation/NavigationComponent/app/src/main/java/com/example/navigationcomponent/LetterListFragment.kt
+++ Navigation/NavigationComponent/app/src/main/java/com/example/navigationcomponent/LetterListFragment.kt
@@ -1,32 +1,26 @@
 package com.example.navigationcomponent
 
 import android.os.Bundle
+import android.view.*
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.navigationcomponent.Adapter.LetterAdapter
+import com.example.navigationcomponent.databinding.FragmentLetterListBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [LetterListFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class LetterListFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private var isLinearLayout = true
+    //binding is null due to inflation of the layout until onCreateView is called
+    private var _binding: FragmentLetterListBinding? = null
+    //append !! if the value won't be null when accessed
+    private val binding get () = _binding!!
+    private lateinit var recyclerView: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+        setHasOptionsMenu(true)
     }
 
     override fun onCreateView(
@@ -34,26 +28,55 @@ class LetterListFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_letter_list, container, false)
+        _binding = FragmentLetterListBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment LetterListFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            LetterListFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        recyclerView = binding.recyclerView
+        chooseLayout()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.layout_menu, menu)
+        val layoutButton = menu.findItem(R.id.action_switch_layout)
+        setIcon(layoutButton)
+    }
+
+    private fun chooseLayout(){
+        if(isLinearLayout){
+            recyclerView.layoutManager = LinearLayoutManager(this)
+        }
+        else{
+            recyclerView.layoutManager = GridLayoutManager(this, 4)
+        }
+
+        recyclerView.adapter = LetterAdapter()
+    }
+
+    private fun setIcon(menuItem: MenuItem?){
+        if(menuItem == null)
+            return
+        menuItem.icon = if(isLinearLayout)
+            ContextCompat.getDrawable(this, R.drawable.ic_linear_layout)
+        else
+            ContextCompat.getDrawable(this, R.drawable.ic_grid_layout)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId){
+            R.id.action_switch_layout -> {
+                isLinearLayout = !isLinearLayout
+                chooseLayout()
+                setIcon(item)
+                return true
             }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
