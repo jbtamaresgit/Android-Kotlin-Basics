6aa0aea518805eb83504f0f3d9351033c318a690
jbtamaresgit <jbtamaresgit@outlook.com>
2021-11-16 15:32:36 +0800

    *bind fragment pick up.xml to order vm variables *update init value with reset order function in order vm

--- Navigation/AdvanceNavigation/app/src/main/java/com/example/advancenavigation/viewmodel/OrderViewModel.kt
+++ Navigation/AdvanceNavigation/app/src/main/java/com/example/advancenavigation/viewmodel/OrderViewModel.kt
@@ -3,18 +3,20 @@ package com.example.advancenavigation.viewmodel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import java.text.SimpleDateFormat
+import java.util.*
 
 class OrderViewModel : ViewModel() {
-    private val _quantity = MutableLiveData<Int>(0)
+    private val _quantity = MutableLiveData<Int>()
     val quantity: LiveData<Int> = _quantity
 
-    private val _flavor = MutableLiveData<String>("")
+    private val _flavor = MutableLiveData<String>()
     val flavor: LiveData<String> = _flavor
 
-    private val _date = MutableLiveData<String>("")
+    private val _date = MutableLiveData<String>()
     val date: LiveData<String> = _date
 
-    private val _price = MutableLiveData<Double>(0.0)
+    private val _price = MutableLiveData<Double>()
     val price: LiveData<Double> = _price
 
     fun setQuantity(numberCupcakes: Int){
@@ -31,5 +33,31 @@ class OrderViewModel : ViewModel() {
 
     fun hasNoFlavourSet(): Boolean{
         return _flavor.value.isNullOrEmpty()
+    }
+
+    init{
+        resetOrder()
+    }
+
+    val dateOptions = getPickupOptions()
+
+    private fun getPickupOptions() : List<String>{
+        val options = mutableListOf<String>()
+        val formatter = SimpleDateFormat("E MMM d", Locale.getDefault())
+        val calendar = Calendar.getInstance()
+        //creates list of dates with the current and the ff 3 dates
+        repeat(4){
+            options.add(formatter.format(calendar.time))
+            calendar.add(Calendar.DATE, 1)
+        }
+
+        return options
+    }
+
+    fun resetOrder(){
+        _quantity.value = 0
+        _flavor.value = ""
+        _date.value = dateOptions[0]
+        _price.value = 0.0
     }
 }
