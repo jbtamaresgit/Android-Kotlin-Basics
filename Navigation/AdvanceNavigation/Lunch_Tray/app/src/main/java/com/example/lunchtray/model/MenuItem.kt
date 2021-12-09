1bdd3fbc08308eb1f53ae1b34e309f4395bce582
jbtamaresgit <jbtamaresgit@outlook.com>
2021-12-09 16:41:23 +0800

    * implement dataSource, dataClass, and ItemType

---
+++ Navigation/AdvanceNavigation/Lunch_Tray/app/src/main/java/com/example/lunchtray/model/MenuItem.kt
@@ -0,0 +1,13 @@
+package com.example.lunchtray.model
+
+import java.text.NumberFormat
+
+data class MenuItem(
+        val name: String,
+        val description: String,
+        val price: Double,
+        val type: Int
+){
+    fun getFormattedPrice(): String = NumberFormat.getCurrencyInstance().format(price)
+}
+
