8ea42857c2f0ed8e4eea32ed6ccac8b811ce3d45
jbtamaresgit <jbtamaresgit@outlook.com>
2021-12-14 13:58:36 +0800

    add default values

--- Navigation/AdvanceNavigation/Lunch_Tray/app/src/main/java/com/example/lunchtray/model/OrderViewModel.kt
+++ Navigation/AdvanceNavigation/Lunch_Tray/app/src/main/java/com/example/lunchtray/model/OrderViewModel.kt
@@ -1,4 +1,16 @@
 package com.example.lunchtray.model
 
-class OrderViewModel {
+import androidx.lifecycle.ViewModel
+import com.example.lunchtray.data.DataSource
+
+
+class OrderViewModel : ViewModel() {
+
+    //map of menu items
+    val menu = DataSource.menuItems
+
+    //default values of price
+    private var previousEntreePrice = 0.0
+    private var previousSidePrice = 0.0
+    private var previousAccompanimentPrice = 0.0
 }
